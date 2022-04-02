86e5ec79076ed9ffcd822d529961c68fd622e55f
Arie Milner <amilner42@gmail.com>
2022-04-02 14:19:08 -0700

    Fix bug with our linking being incorrect from Todo.Cache to Todo.Database

--- lib/todo/cache.ex
+++ lib/todo/cache.ex
@@ -5,7 +5,21 @@ defmodule Todo.Cache do
 
   def start_link(_) do
     IO.puts("Starting to-do cache with `start_link`...")
-    Todo.Database.start_link()
+    # Doing this lead to a very confusing bug where killing the worker with:
+    #
+    #  Process.exit(Process.whereis(Todo.Database), :asdf)
+    #
+    # Did not reboot everything! Because it was linking it to the calling process
+    # which must have been the supervisor, who, must have ignored the death
+    # given it is not a direct child.
+    #
+    # To add to my confusion, when starting without the supervisor, killing
+    # the database did kill everything making me feel it was indeed connected
+    # and it was a supervisor-issue. By directly calling start_link from the
+    # repl, it must be the repl process that "connected" everything and
+    # therefore they all crash when one crashes.
+    #
+    # Todo.Database.start_link()
     GenServer.start_link(__MODULE__, nil, name: __MODULE__)
   end
 
@@ -17,7 +31,14 @@ defmodule Todo.Cache do
 
   @impl GenServer
   def init(_) do
+    send(self(), :real_init)
     {:ok, %{}}
+  end
+
+  @impl GenServer
+  def handle_info(:real_init, state) do
+    Todo.Database.start_link()
+    {:noreply, state}
   end
 
   @impl GenServer

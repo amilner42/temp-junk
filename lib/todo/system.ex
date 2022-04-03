b4f8d673dc82ce2ff79a157a769c23ba4fe976c8
Arie Milner <amilner42@gmail.com>
2022-04-03 08:51:47 -0700

    use 1-1 strategy

--- lib/todo/system.ex
+++ lib/todo/system.ex
@@ -2,7 +2,7 @@ defmodule Todo.System do
   def start_link do
     Supervisor.start_link(
       [Todo.Database, Todo.Cache],
-      strategy: :one_for_all
+      strategy: :one_for_one
     )
   end
 end

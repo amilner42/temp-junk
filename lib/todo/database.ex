c842f2e2f35ebc15626800c236f90b67c2d5e667
Arie Milner <amilner42@gmail.com>
2022-04-10 00:04:56 -0700

    Use poolboy

--- lib/todo/database.ex
+++ lib/todo/database.ex
@@ -6,47 +6,45 @@ defmodule Todo.Database do
 
   def store(key, data) do
     file_location = file_location_for_key(key)
-    worker_index = worker_index_for_key(key)
 
-    Todo.DatabaseWorker.store(worker_index, file_location, data)
+    :poolboy.transaction(
+      __MODULE__,
+      fn worker_pid ->
+        Todo.DatabaseWorker.store(worker_pid, file_location, data)
+      end
+    )
   end
 
   def get(key) do
     file_location = file_location_for_key(key)
-    worker_index = worker_index_for_key(key)
 
-    Todo.DatabaseWorker.get(worker_index, file_location)
+    :poolboy.transaction(
+      __MODULE__,
+      fn worker_pid ->
+        Todo.DatabaseWorker.get(worker_pid, file_location)
+      end
+    )
   end
 
   # Module Callback Methods
 
-  defp worker_spec(worker_id) do
-    default_worker_spec = {Todo.DatabaseWorker, worker_id}
-    Supervisor.child_spec(default_worker_spec, id: worker_id)
-  end
-
   def child_spec(_) do
-    %{
-      id: __MODULE__,
-      start: {__MODULE__, :start_link, []},
-      type: :supervisor
-    }
-  end
-
-  def start_link do
     File.mkdir_p!(@db_folder)
 
-    children = Enum.map(1..@db_worker_count, &worker_spec/1)
-    Supervisor.start_link(children, strategy: :one_for_one)
+    :poolboy.child_spec(
+      __MODULE__,
+      [
+        name: {:local, __MODULE__},
+        worker_module: Todo.DatabaseWorker,
+        size: @db_worker_count
+      ],
+      [nil]
+    )
   end
 
   # Private Helpers
 
   defp file_location_for_key(key) do
     Path.join(@db_folder, to_string(key))
-  end
-
-  defp worker_index_for_key(key) do
-    :erlang.phash2(key, @db_worker_count) + 1
   end
 end
